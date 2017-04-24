e99afff0ff0dd74ba60638168890f9b7935dded1
Megan Smith <meg.eli.smi@gmail.com>
2017-04-24 14:17:58 -0700

    milligram css set up and basic table displaying

--- packages/wheelhouse-frontend/src/App.js
+++ packages/wheelhouse-frontend/src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-// import "milligram.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+     <table className="reusable-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Height</th>
+            <th>Location</th>
+          </tr>
+      </thead>
+    <tbody>
+      <tr>
+        <td>Stephen Curry</td>
+        <td>27</td>
+        <td>1,91</td>
+        <td>Akron, OH</td>
+      </tr>
+      <tr>
+        <td>Klay Thompson</td>
+        <td>25</td>
+        <td>2,01</td>
+        <td>Los Angeles, CA</td>
+      </tr>
+    </tbody>
+  </table>
     );
   }
 }
