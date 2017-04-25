5f9f4c024bb98d313da35c181afd7d7a593e99c6
Megan Smith <meg.eli.smi@gmail.com>
2017-04-25 15:51:48 -0700

    favicon and logo added

--- packages/wheelhouse-frontend/src/components/reusables/Header.js
+++ packages/wheelhouse-frontend/src/components/reusables/Header.js
@@ -1,9 +1,17 @@
 import React from "react"; 
+import "../../Header.css";
+// import icon from "../../../public/wheelhouse-icon.png";
 
 const Header = () => {
   return (
-    <h1 className="app-header">Wheelhouse</h1>
+    <div className="container">
+      <div className="row">
+        <div className="column"><h1 className="app-header">Wheelhouse</h1></div>
+      </div>
+    </div>
   );
 }; 
 
 export default Header; 
+
+//<div className="column column-90"><img className="icon" src={icon} alt="icon" /></div>
