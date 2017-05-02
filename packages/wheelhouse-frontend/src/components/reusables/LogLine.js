6fa8d4f78da31231bb82861e3b71be77806e4c88
megelismi <meg.eli.smi@gmail.com>
2017-05-01 21:39:43 -0700

    Logs polish (#25)
    * fixed eslint errors
    
    * slider buttons filtering logs
    
    * basic formatting for filter buttons
    
    * show all, hide all buttons working on filter log feature
    
    * log container refactored to be reusuable, connected to development page
    
    * logs showing up for individual apps on development page
    
    * bug fixed so that you can see individual logs at the same time, doesn't all auto populate to last opened log
    
    * individual logs on development page update as redux store updates
    
    * filter buttons reformatted, some code refactored in LogsDataDisplay
    
    * comment added about reusability of code in componentWillMount and componentDidMount
    
    * debugger taken out of reducer
    
    * pod-handlers file added
    
    * changes made from pull request

--- packages/wheelhouse-frontend/src/components/reusables/LogLine.js
+++ packages/wheelhouse-frontend/src/components/reusables/LogLine.js
@@ -3,9 +3,9 @@ import React from "react";
 const LogLine = (props) => {
   return (
     <div className="log-line-container">
-      <div className="log-line-row">
+      <div className="log-line-row" style={props.display}>
         <div className="log-line-column nowrap">{props.timeStamp}</div>
-        <div className="log-line-column log-line-app-name nowrap" style={props.customStyle}>{props.appName}</div>
+        <div className="log-line-column log-line-app-name nowrap" style={props.color}>{props.appName}</div>
         <div className="log-line-column log-line-app-server nowrap">{props.serverStatus}</div>
         <div className="log-line-column">{props.expectedAction} </div>
       </div>
