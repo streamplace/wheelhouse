cffad9051c418649c5f35b38827b70435662bb5f
Eli Mallon <eli@iame.li>
2017-05-11 12:19:31 -0700

    refactor run.js

--- packages/wheelhouse/src/util/run.js
+++ packages/wheelhouse/src/util/run.js
@@ -1,35 +1,54 @@
+/**
+ * This file is filled with helpers for running external processes. We do a lot of that in
+ * Wheelhouse.
+ */
+import { resolve, dirname } from "path";
+import { spawn } from "child_process";
+
 let runningProcs = [];
-// export function run(cmd, ...args)
 
-export const runKube = (...args) => {
-  const spawn = require("child_process").spawn;
-  const kubeProc = spawn("kubectl", args);
-  runningProcs.push(kubeProc);
+export const run = (cmd, ...args) => {
+  const proc = spawn(cmd, args);
 
-  let output = "";
-  let errorOutput = "";
+  runningProcs.push(proc);
 
-  kubeProc.stdout.on("data", data => {
-    output += data;
+  let stdout = "";
+  let stderr = "";
+
+  proc.stdout.on("data", data => {
+    stdout += data;
   });
 
-  kubeProc.stderr.on("data", data => {
-    errorOutput += data;
+  proc.stderr.on("data", data => {
+    stderr += data;
   });
+
+  let dead = false;
 
   const prom = new Promise((resolve, reject) => {
-    kubeProc.on("close", code => {
-      runningProcs = runningProcs.filter(p => p !== kubeProc);
+    proc.on("close", code => {
+      if (dead) {
+        return;
+      }
+      dead = true;
+      runningProcs = runningProcs.filter(p => p !== proc);
       if (code === 0) {
-        resolve(output);
+        resolve(stdout);
       } else {
-        reject(errorOutput);
+        reject(stderr);
       }
     });
   });
 
+  // A bit hacky, but we provide "exit" on these handles so they can be terminated by whoever
+  // requested them. What's a better way to handle this? Perhaps opaque tokens that get passed back
+  // to this module, in the same style as setTimeout?
   prom.exit = function() {
-    kubeProc.kill("KILL");
+    if (dead) {
+      return;
+    }
+    dead = true;
+    proc.kill("KILL");
   };
 
   return prom;
@@ -38,3 +57,10 @@ export const runKube = (...args) => {
 process.on("SIGTERM", () => {
   runningProcs.forEach(proc => proc.kill("KILL"));
 });
+
+export const runKube = (...args) => {
+  const kubePath = resolve(dirname(require.resolve("kubectl-cli")), "kubectl");
+  return run(kubePath, ...args).then(data => {
+    return JSON.parse(data);
+  });
+};
