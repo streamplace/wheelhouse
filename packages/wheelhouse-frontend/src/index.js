3d231b3a9aa0e1878e1c4547a7e2e504786d40a2
Eli Mallon <eli@iame.li>
2017-06-02 20:31:42 -0700

    Don't block on FontAwesome
    I am on an airplane. Needed this to make Wheelhouse work.
    
    Really we should be just getting font awesome from npm

--- packages/wheelhouse-frontend/src/index.js
+++ packages/wheelhouse-frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "font-awesome/css/font-awesome.css";
 import getStore from "./store";
 import App from "./app";
 import { Provider } from "react-redux";
